9b70ce9aacef5c961990bfa9278d4f7e5e6ea554
kadrij <jade.kadri@epitech.eu>
2018-03-05 22:13:31 +0100

    Activité Register terminé

--- app/src/main/java/com/eip/stopclopeip/LoginActivity.java
+++ app/src/main/java/com/eip/stopclopeip/LoginActivity.java
@@ -2,14 +2,11 @@ package com.eip.stopclopeip;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Typeface;
 
-import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.WindowManager;
@@ -32,12 +29,11 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import java.util.Map;
 
-public class LoginActivity extends Activity
-{
-    private AutoCompleteTextView mEmailView;
-    private EditText mPasswordView;
-    private ProgressBar mProgressView;
-    private ScrollView mLoginFormView;
+public class LoginActivity extends Activity {
+    private AutoCompleteTextView mEmail;
+    private EditText mPassword;
+    private ProgressBar mProgress;
+    private ScrollView mLoginForm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,10 +43,10 @@ public class LoginActivity extends Activity
 
         final String url = "http://romain-caldas.fr/api/rest.php?dev=69";
 
-        mProgressView = findViewById(R.id.login_progress_bar);
-        mLoginFormView = findViewById(R.id.login_form);
-        mEmailView = findViewById(R.id.email_input);
-        mPasswordView = findViewById(R.id.password_input);
+        mProgress = findViewById(R.id.login_progress_bar);
+        mLoginForm = findViewById(R.id.login_form);
+        mEmail = findViewById(R.id.email_input);
+        mPassword = findViewById(R.id.password_input);
 
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
         final Boolean[] logError = {false};
@@ -64,79 +60,72 @@ public class LoginActivity extends Activity
             @Override
             public void onClick(View view) {
                 showProgress(true);
-
-                Log.v("Login", "Email = " + mEmailView.getText().toString() + " | Password = " + mPasswordView.getText().toString());
-
-                if (mEmailView.getText().toString().isEmpty() && mPasswordView.getText().toString().isEmpty() && logError[0].equals(false)) {
-                    mEmailView.setError("An Email or Username is requiered");
-                    mPasswordView.setError("A password is requiered");
+                if (mEmail.getText().toString().isEmpty() && mPassword.getText().toString().isEmpty() && logError[0].equals(false)) {
+                    mEmail.setError("An Email or Username is requiered");
+                    mPassword.setError("A password is requiered");
                     Alert("Login and password requiered.");
                     showProgress(false);
                     logError[0] = true;
-                } else if (mEmailView.getText().toString().isEmpty() && !mPasswordView.getText().toString().isEmpty() && logError[0].equals(false)) {
-                    mEmailView.setError("An Email or Username is requiered");
+                } else if (mEmail.getText().toString().isEmpty() && !mPassword.getText().toString().isEmpty() && logError[0].equals(false)) {
+                    mEmail.setError("An Email or Username is requiered");
                     Alert("An Email or Username is requiered.");
                     showProgress(false);
                     logError[0] = true;
-                } else if (!mEmailView.getText().toString().isEmpty() && mPasswordView.getText().toString().isEmpty() && logError[0].equals(false)) {
-                    mPasswordView.setError("A password is requiered");
+                } else if (!mEmail.getText().toString().isEmpty() && mPassword.getText().toString().isEmpty() && logError[0].equals(false)) {
+                    mPassword.setError("A password is requiered");
                     Alert("A password is requiered.");
                     showProgress(false);
                     logError[0] = true;
-                }
+                } else {
+                    StringRequest stringRequest = new StringRequest(Request.Method.GET, url
+                            + "&function=user.connection&email="
+                            + mEmail.getText().toString()
+                            + "&password="
+                            + mPassword.getText().toString(), new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            JSONObject jsonResponse = null;
 
-                StringRequest stringRequest = new StringRequest(Request.Method.GET, url
-                        + "&function=user.connection&email="
-                        + mEmailView.getText().toString()
-                        + "&password="
-                        + mPasswordView.getText().toString(), new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        JSONObject jsonResponse = null;
-
-                        try {
-                            jsonResponse = new JSONObject(response);
-                        }
-                        catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-
-                        try {
-                            JSONObject jsonData = new JSONObject(jsonResponse.getString("data"));
-                            if (jsonResponse.getString("reponse").equals("OK")) {
-                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                                intent.putExtra("token", jsonData.getString("token"));
-                                intent.putExtra("email", mEmailView.getText().toString());
-                                showProgress(false);
-                                startActivity(intent);
+                            try {
+                                jsonResponse = new JSONObject(response);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
                             }
-                            else {
-                                mEmailView.setError("Adresse mail ou mot de passe incorrecte");
+
+                            try {
+                                JSONObject jsonData = new JSONObject(jsonResponse.getString("data"));
+                                if (jsonResponse.getString("reponse").equals("OK")) {
+                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                    intent.putExtra("token", jsonData.getString("token"));
+                                    intent.putExtra("email", mEmail.getText().toString());
+                                    showProgress(false);
+                                    startActivity(intent);
+                                } else {
+                                    mEmail.setError("Adresse mail ou mot de passe incorrecte");
+                                    showProgress(false);
+                                }
+                            } catch (JSONException e) {
+                                Alert("Cannot connect to server.");
                                 showProgress(false);
+                                e.printStackTrace();
                             }
                         }
-                        catch (JSONException e) {
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
                             Alert("Cannot connect to server.");
                             showProgress(false);
-                            e.printStackTrace();
                         }
-                    }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Alert("Cannot connect to server.");
-                        showProgress(false);
-                    }
-                })
-                {
-                    @Override
-                    protected Map<String, String> getParams() {
-                        return null;
-                    }
-                };
-                queue.add(stringRequest);
-                queue.start();
-                logError[0] = false;
+                    }) {
+                        @Override
+                        protected Map<String, String> getParams() {
+                            return null;
+                        }
+                    };
+                    queue.add(stringRequest);
+                    queue.start();
+                    logError[0] = false;
+                }
             }
         });
     }
@@ -144,21 +133,21 @@ public class LoginActivity extends Activity
     void showProgress(final boolean show) {
         int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
 
-        mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-        mLoginFormView.animate().setDuration(shortAnimTime).alpha(
+        mLoginForm.setVisibility(show ? View.GONE : View.VISIBLE);
+        mLoginForm.animate().setDuration(shortAnimTime).alpha(
                 show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
             @Override
             public void onAnimationEnd(Animator animation) {
-                mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                mLoginForm.setVisibility(show ? View.GONE : View.VISIBLE);
             }
         });
 
-        mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-        mProgressView.animate().setDuration(shortAnimTime).alpha(
+        mProgress.setVisibility(show ? View.VISIBLE : View.GONE);
+        mProgress.animate().setDuration(shortAnimTime).alpha(
                 show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
             @Override
             public void onAnimationEnd(Animator animation) {
-                mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                mProgress.setVisibility(show ? View.VISIBLE : View.GONE);
             }
         });
     }
