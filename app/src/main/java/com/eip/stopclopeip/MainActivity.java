769f8388a01b71a771c32c0f348723ce1d6a6350
kadrij <jade.kadri@epitech.eu>
2018-03-10 00:54:32 +0100

    Mise en place de la liaison entre l'objet connecté et l'application

--- app/src/main/java/com/eip/stopclopeip/MainActivity.java
+++ app/src/main/java/com/eip/stopclopeip/MainActivity.java
@@ -6,28 +6,46 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends BlunoLibrary implements NavigationView.OnNavigationItemSelectedListener {
+    String url = "http://romain-caldas.fr/api/rest.php?dev=69";
+    boolean onButtonFragment = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        onCreateProcess();
+        serialBegin(115200);
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -68,7 +86,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         int id = item.getItemId();
 
         if (id == R.id.action_scan) {
-            return true;
+            buttonScanOnClickProcess();
         }
 
         return super.onOptionsItemSelected(item);
@@ -78,6 +96,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         int id = item.getItemId();
+        onButtonFragment = false;
         Toolbar toolbar = findViewById(R.id.toolbar);
         FragmentManager fragmentManager = getFragmentManager();
         Bundle bundle = new Bundle();
@@ -90,7 +109,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             mFragment.setArguments(bundle);
             fragmentManager.beginTransaction().replace(R.id.content_frame, mFragment).commit();
         } else if (id == R.id.nav_button) {
-            toolbar.setTitle("Bouton");
+            toolbar.setTitle("Boutons");
+            onButtonFragment = true;
             ButtonFragment mFragment = new ButtonFragment();
             mFragment.setArguments(bundle);
             fragmentManager.beginTransaction().replace(R.id.content_frame, mFragment).commit();
@@ -108,6 +128,107 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void sendObjectPression(String color) {
+        final RequestQueue queue = Volley.newRequestQueue(this);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url
+                + "&function=coordonnee.add&email="
+                + getIntent().getStringExtra("email")
+                + "&token="
+                + getIntent().getStringExtra("token")
+                + "&x=2&y=2&button="
+                + color,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        JSONObject jsonResponse = null;
+                        try {
+                            jsonResponse = new JSONObject(response);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Alert("Impossible de se connecter au serveur");
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap();
+                params.put("myData", "{}");
+                return params;
+            }
+        };
+        queue.add(stringRequest);
+        queue.start();
+    }
+
+    private void getPressionCount() {
+        final RequestQueue queue = Volley.newRequestQueue(this);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url
+                + "&function=coordonnee.get&email="
+                + getIntent().getStringExtra("email")
+                + "&token="
+                + getIntent().getStringExtra("token")
+                + "&time=1j",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        JSONObject jsonResponse = null;
+                        try {
+                            jsonResponse = new JSONObject(response);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            TextView red_count = findViewById(R.id.red_count);
+                            TextView blue_count = findViewById(R.id.blue_count);
+                            TextView black_count = findViewById(R.id.black_count);
+
+                            String data = jsonResponse.getString("data");
+                            JSONArray userArray = new JSONArray(data);
+
+                            int red = 0;
+                            int blue = 0;
+                            int black = 0;
+
+                            for (int i = 0; i < userArray.length(); i++) {
+                                JSONObject userData = userArray.getJSONObject(i);
+                                if (userData.getString("button").equals("BLUE"))
+                                    blue++;
+                                else if (userData.getString("button").equals("RED"))
+                                    red++;
+                                else
+                                    black++;
+                            }
+
+                            red_count.setText("" + red);
+                            blue_count.setText("" + blue);
+                            black_count.setText("" + black);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Alert("Impossible de se connecter au serveur");
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap();
+                params.put("myData", "{}");
+                return params;
+            }
+        };
+        queue.add(stringRequest);
+        queue.start();
     }
 
     public void showNotification(String Msg) {
@@ -135,5 +256,65 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     public void Alert(String Msg) {
         Toast.makeText(this, Msg, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        onResumeProcess();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        onActivityResultProcess(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        onPauseProcess();
+    }
+
+    protected void onStop() {
+        super.onStop();
+        onStopProcess();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        onDestroyProcess();
+    }
+
+    @Override
+    public void onConectionStateChange(BlunoLibrary.connectionStateEnum theConnectionState) {
+        switch (theConnectionState) {
+            case isConnected:
+                break;
+            case isConnecting:
+                break;
+            case isToScan:
+                break;
+            case isScanning:
+                break;
+            case isDisconnecting:
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onSerialReceived(String theString) {
+        if (theString.equals("red"))
+            sendObjectPression("RED");
+        else if (theString.equals("blue"))
+            sendObjectPression("BLUE");
+        else if (theString.equals("black"))
+            sendObjectPression("BLACK");
+
+        if (onButtonFragment == true)
+            getPressionCount();
     }
 }
