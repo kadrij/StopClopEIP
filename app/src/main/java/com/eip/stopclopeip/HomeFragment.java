c9ec78989654226ceb373090258dc7e19d0f6f9a
kadrij <jade.kadri@epitech.eu>
2018-03-14 02:47:14 +0100

    Mise en place du temps de sevrage ainsi que du record sur l'application

--- app/src/main/java/com/eip/stopclopeip/HomeFragment.java
+++ app/src/main/java/com/eip/stopclopeip/HomeFragment.java
@@ -2,9 +2,35 @@ package com.eip.stopclopeip;
 
 import android.os.Bundle;
 import android.app.Fragment;
+import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Chronometer;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import javax.security.auth.callback.Callback;
 
 public class HomeFragment extends Fragment {
     String url = "http://romain-caldas.fr/api/rest.php?dev=69";
@@ -26,4 +52,161 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
+
+    @Override
+    public void onViewCreated(View view, Bundle savecInstanceState) {
+        final ImageView cactus = view.findViewById(R.id.cactos_image);
+        Chronometer sevrageTime = view.findViewById(R.id.sevrage_value);
+        TextView sevrageRecord = view.findViewById(R.id.record_value);
+        final Date[] mDateClop = {new Date()};
+
+        getDateClop(new CallBack() {
+            @Override
+            public void onSuccess(Date dateClop) {
+                mDateClop[0] = dateClop;
+            }
+
+            @Override
+            public void onFail(String msg) {
+                Alert("Impossible de récupérer le temps de sevrage");
+            }
+        });
+
+        sevrageTime.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+            @Override
+            public void onChronometerTick(Chronometer chronometer) {
+                Date currentDate = new Date();
+                long diff = Math.abs(currentDate.getTime() - mDateClop[0].getTime());
+                if (TimeUnit.MILLISECONDS.toHours(diff) < 10)
+                    cactus.setImageResource(R.drawable.cactos1);
+                else if (TimeUnit.MILLISECONDS.toHours(diff) >= 10 && TimeUnit.MILLISECONDS.toHours(diff) < 48)
+                    cactus.setImageResource(R.drawable.cactos2);
+                else if (TimeUnit.MILLISECONDS.toHours(diff) >= 48 && TimeUnit.MILLISECONDS.toHours(diff) < 168)
+                    cactus.setImageResource(R.drawable.cactos3);
+                else if (TimeUnit.MILLISECONDS.toHours(diff) >= 168)
+                    cactus.setImageResource(R.drawable.cactos4);
+                chronometer.setText(String.format("%02d : %02d : %02d",
+                        TimeUnit.MILLISECONDS.toDays(diff),
+                        TimeUnit.MILLISECONDS.toHours(diff) % 24,
+                        TimeUnit.MILLISECONDS.toMinutes(diff) % 60));
+            }
+        });
+        sevrageTime.start();
+        getRecord();
+    }
+
+    public void getRecord() {
+        RequestQueue queue = Volley.newRequestQueue(this.getActivity());
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url
+                + "&function=sevrage.record&email="
+                + getArguments().getString("email")
+                + "&token="
+                + getArguments().getString("token"),
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        JSONObject jsonResponse = null;
+                        try {
+                            jsonResponse = new JSONObject(response);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            JSONObject jsonData = new JSONObject(jsonResponse.getString("data"));
+                            JSONObject jsonDate1 = new JSONObject(jsonData.getString("date1"));
+                            JSONObject jsonDate2 = new JSONObject(jsonData.getString("date2"));
+                            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSSSS");
+                            try {
+                                TextView recordValue = getView().findViewById(R.id.record_value);
+
+                                final Date date1 = format.parse(jsonDate1.getString("date"));
+                                final Date date2 = format.parse(jsonDate2.getString("date"));
+                                long diff = Math.abs(date2.getTime() - date1.getTime());
+
+                                recordValue.setText(String.format("%02d : %02d : %02d",
+                                        TimeUnit.MILLISECONDS.toDays(diff),
+                                        TimeUnit.MILLISECONDS.toHours(diff) % 24,
+                                        TimeUnit.MILLISECONDS.toMinutes(diff) % 60));
+                            } catch (ParseException e) {
+                                e.printStackTrace();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Alert("Impossible de se connecter au serveur");
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap();
+                params.put("myData", "{}");
+                return params;
+            }
+        };
+        queue.add(stringRequest);
+        queue.start();
+    }
+
+    public void getDateClop(final CallBack onCallBack) {
+        RequestQueue queue = Volley.newRequestQueue(this.getActivity());
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url
+                + "&function=sevrage.time&email="
+                + getArguments().getString("email")
+                + "&token="
+                + getArguments().getString("token")
+                + "&button=BLACK",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        JSONObject jsonResponse = null;
+                        try {
+                            jsonResponse = new JSONObject(response);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            JSONObject jsonData = new JSONObject(jsonResponse.getString("data"));
+                            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                            try {
+                                final Date dateClop = format.parse(jsonData.getString("dateClop"));
+                                onCallBack.onSuccess(dateClop);
+                            } catch (ParseException e) {
+                                e.printStackTrace();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Alert("Impossible de se connecter au serveur");
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap();
+                params.put("myData", "{}");
+                return params;
+            }
+        };
+        queue.add(stringRequest);
+        queue.start();
+    }
+
+    public interface CallBack {
+        void onSuccess(Date dateClop);
+        void onFail(String msg);
+    }
+
+    public void Alert(String Msg) {
+        Toast.makeText(this.getActivity(), Msg, Toast.LENGTH_SHORT).show();
+    }
 }
+
