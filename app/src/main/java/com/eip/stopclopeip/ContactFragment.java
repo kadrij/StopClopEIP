a4ed43682f035668d180129c72bfaadbcacb2b6e
kadrij <jade.kadri@epitech.eu>
2018-09-17 19:36:45 +0200

    Mise en place de la carte totalement fonctionnel

--- app/src/main/java/com/eip/stopclopeip/ContactFragment.java
+++ app/src/main/java/com/eip/stopclopeip/ContactFragment.java
@@ -1,14 +1,12 @@
 package com.eip.stopclopeip;
 
-import android.annotation.SuppressLint;
-import android.content.Context;
+import android.Manifest;
 import android.content.pm.PackageManager;
-import android.location.Criteria;
 import android.location.Location;
-import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,7 +14,6 @@ import android.view.ViewGroup;
 import com.google.android.gms.location.places.GeoDataClient;
 import com.google.android.gms.location.places.PlaceDetectionClient;
 import com.google.android.gms.location.places.Places;
-import com.google.android.gms.location.places.PlaceDetectionApi;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -28,7 +25,11 @@ import com.google.android.gms.maps.model.LatLng;
 public class ContactFragment extends Fragment implements OnMapReadyCallback {
     protected GeoDataClient mGeoDataClient;
     protected PlaceDetectionClient mPlaceDetectionClient;
-    GoogleMap mGoogleMap;
+    public static final int REQUEST_LOCATION_CODE = 99;
+    int PROXIMITY_RADIUS = 20000;
+    private GoogleMap mMap;
+    double latitude;
+    double longitude;
     MapView mMapView;
     View mView;
 
@@ -66,23 +67,59 @@ public class ContactFragment extends Fragment implements OnMapReadyCallback {
         mPlaceDetectionClient = Places.getPlaceDetectionClient(getActivity().getBaseContext(), null);
     }
 
-    @SuppressLint("MissingPermission")
     @Override
     public void onMapReady(final GoogleMap googleMap) {
+        mMap = googleMap;
         final Boolean[] init = {false};
+        final GetNearbyPlacesData getNearbyPlacesData = new GetNearbyPlacesData(getActivity());
+        checkLocationPermission();
         MapsInitializer.initialize(getActivity().getBaseContext());
-        mGoogleMap = googleMap;
-        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        googleMap.setMyLocationEnabled(true);
-        googleMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.setMyLocationEnabled(true);
+
+        mMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
             @Override
             public void onMyLocationChange(Location arg0) {
+                latitude = arg0.getLatitude();
+                longitude = arg0.getLongitude();
                 if (init[0] == false) {
-                    CameraPosition pos = CameraPosition.builder().target(new LatLng(arg0.getLatitude(), arg0.getLongitude())).zoom(15).bearing(0).tilt(0).build();
-                    googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(pos));
+                    CameraPosition pos = CameraPosition.builder().target(new LatLng(arg0.getLatitude(), arg0.getLongitude())).zoom(12).bearing(0).tilt(0).build();
+                    mMap.moveCamera(CameraUpdateFactory.newCameraPosition(pos));
+                    getPharmacies(getNearbyPlacesData);
                     init[0] = true;
                 }
             }
         });
+
+        MapsInitializer.initialize(getActivity().getBaseContext());
+    }
+
+    private void getPharmacies(GetNearbyPlacesData getNearbyPlacesData) {
+        String url = getUrl(latitude, longitude);
+        Object dataTransfer[] = new Object[2];
+        dataTransfer[0] = mMap;
+        dataTransfer[1] = url;
+        getNearbyPlacesData.execute(dataTransfer);
+    }
+
+    private String getUrl(double latitude, double longitude) {
+        StringBuilder googlePlaceUrl = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+        googlePlaceUrl.append("location=" + latitude + "," + longitude);
+        googlePlaceUrl.append("&radius=" + PROXIMITY_RADIUS);
+        googlePlaceUrl.append("&type=pharmacy");
+        googlePlaceUrl.append("&sensor=true");
+        googlePlaceUrl.append("&key=AIzaSyAMMlC-L-DD2n08cSuNnoukKLVNzqJzn_U");
+        return googlePlaceUrl.toString();
+    }
+
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(getActivity().getBaseContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity().getParent(), Manifest.permission.ACCESS_FINE_LOCATION))
+                ActivityCompat.requestPermissions(getActivity().getParent(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION_CODE);
+            else
+                ActivityCompat.requestPermissions(getActivity().getParent(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION_CODE);
+            return false;
+        } else
+            return true;
     }
 }
--- app/src/main/java/com/eip/stopclopeip/DownloadUrl.java
+++ app/src/main/java/com/eip/stopclopeip/DownloadUrl.java
@@ -1,4 +1,44 @@
 package com.eip.stopclopeip;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class DownloadUrl {
+    public String readUrl(String mUrl) throws IOException {
+        String data = "";
+        InputStream inputStream = null;
+        HttpURLConnection urlConnection = null;
+
+        try {
+            URL url = new URL(mUrl);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.connect();
+
+            inputStream = urlConnection.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+            StringBuffer sb = new StringBuffer();
+
+            String line = "";
+            while ((line = br.readLine()) != null)
+                sb.append(line);
+
+            data = sb.toString();
+            br.close();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            inputStream.close();
+            urlConnection.disconnect();
+        }
+
+        return data;
+    }
 }
